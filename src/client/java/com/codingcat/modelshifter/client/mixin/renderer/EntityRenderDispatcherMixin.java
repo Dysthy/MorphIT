003860b6532fe10e007520f83daabcaff7c4e0af
codingcat2468 <lw-app-produktion@gmx.net>
2024-12-27 08:05:56 +0100

    Implement new system in mixins

--- src/client/java/com/codingcat/modelshifter/client/mixin/renderer/EntityRenderDispatcherMixin.java
+++ src/client/java/com/codingcat/modelshifter/client/mixin/renderer/EntityRenderDispatcherMixin.java
@@ -4,11 +4,10 @@ import com.codingcat.modelshifter.client.ModelShifterClient;
 import com.codingcat.modelshifter.client.api.entity.EntityRenderStateWrapper;
 import com.codingcat.modelshifter.client.api.model.ModelDimensions;
 import com.codingcat.modelshifter.client.api.model.PlayerModel;
+import com.codingcat.modelshifter.client.render.ReplacedPlayerEntityRenderer;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
-//? >=1.21.3 {
 import net.minecraft.client.render.entity.state.EntityRenderState;
-//?}
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.Box;
@@ -16,10 +15,51 @@ import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Objects;
 
 @Mixin(EntityRenderDispatcher.class)
 public class EntityRenderDispatcherMixin {
+    // Replaces the vanilla player renderer with a custom one from modelshifter, which will
+    // render the assigned PlayerModel. If no model is active, the vanilla player renderer will be used.
+    @Inject(
+            method = "getRenderer",
+            at = @At("HEAD"),
+            cancellable = true)
+    public void injectGetRenderer(Entity entity, CallbackInfoReturnable<ReplacedPlayerEntityRenderer> cir) {
+        EntityRenderStateWrapper state = EntityRenderStateWrapper.of(entity);
+        if (!state.isPlayer() || !ModelShifterClient.state.isRendererEnabled(Objects.requireNonNull(state.getPlayer()))) return;
+
+        PlayerModel playerModel = ModelShifterClient.state.getState(state.getPlayer().getUuid()).getPlayerModel();
+        cir.setReturnValue(ModelShifterClient.holder.getRenderer(playerModel));
+        cir.cancel();
+    }
+
+    // Debug tool that replaces the vanilla hitbox of a player with a custom one
+    // which shows the scale of the active PlayerModel to make adjusting the values easier.
+    // Will ONLY become active when modelshifter is launched in a fabric development environment!
+    @Redirect(
+            method = "renderHitbox",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;getBoundingBox()Lnet/minecraft/util/math/Box;")
+    )
+    private static Box redirectGetBox(Entity entity) {
+        if (!ModelShifterClient.isDev) return entity.getBoundingBox();
+        if (!(entity instanceof AbstractClientPlayerEntity player)) return entity.getBoundingBox();
+        ModelDimensions dimensions = getDimensions(player);
+        if (dimensions == null) return entity.getBoundingBox();
+
+        return getBox(dimensions.width(), dimensions.height(), entity.getX(), entity.getY(), entity.getZ());
+    }
+
+    @Unique
+    private static Box getBox(float w, float h, double x, double y, double z) {
+        float f = w / 2.0f;
+        return new Box(x - (double) f, y, z - (double) f, x + (double) f, y + (double) h, z + (double) f);
+    }
+
     @Redirect(
             method = "renderFire",
             //? >=1.21.3 {
@@ -50,26 +90,6 @@ public class EntityRenderDispatcherMixin {
         EntityRenderStateWrapper state = EntityRenderStateWrapper.of(e);
         ModelDimensions dimensions = getDimensions(state);
         return dimensions != null ? dimensions.height() : state.getHeight();
-    }
-
-    @Redirect(
-            method = "renderHitbox",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;getBoundingBox()Lnet/minecraft/util/math/Box;")
-    )
-    private static Box redirectGetBox(Entity entity) {
-        if (!ModelShifterClient.isDev) return entity.getBoundingBox();
-        if (!(entity instanceof AbstractClientPlayerEntity player)) return entity.getBoundingBox();
-        ModelDimensions dimensions = getDimensions(player);
-        if (dimensions == null) return entity.getBoundingBox();
-
-        return getBox(dimensions.width(), dimensions.height(), entity.getX(), entity.getY(), entity.getZ());
-    }
-
-
-    @Unique
-    private static Box getBox(float w, float h, double x, double y, double z) {
-        float f = w / 2.0f;
-        return new Box(x - (double) f, y, z - (double) f, x + (double) f, y + (double) h, z + (double) f);
     }
 
     @Unique
--- src/client/java/com/codingcat/modelshifter/client/mixin/renderer/PlayerEntityRendererMixin.java
+++ src/client/java/com/codingcat/modelshifter/client/mixin/renderer/PlayerEntityRendererMixin.java
@@ -1,30 +1,17 @@
 package com.codingcat.modelshifter.client.mixin.renderer;
 
 import com.codingcat.modelshifter.client.ModelShifterClient;
-import com.codingcat.modelshifter.client.api.entity.EntityRenderStateWrapper;
-import com.codingcat.modelshifter.client.api.renderer.DynamicAdditionalRendererHolder;
-import net.minecraft.client.model.ModelPart;
+import com.codingcat.modelshifter.client.api.renderer.AdditionalRendererManager;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
-import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.render.entity.model.PlayerEntityModel;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.math.Vec3d;
-import org.spongepowered.asm.mixin.Mixin;
-//? <1.21.3 {
-/*import com.codingcat.modelshifter.client.render.ReplacedPlayerEntityRenderer;
-import com.codingcat.modelshifter.client.api.model.PlayerModel;
-import com.codingcat.modelshifter.client.util.MixinUtil;
-*///?} else {
 import net.minecraft.client.render.entity.state.PlayerEntityRenderState;
-import net.minecraft.util.Identifier;
-//?}
+import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntityRenderer.class)
 //? >=1.21.3 {
@@ -45,105 +32,7 @@ public abstract class PlayerEntityRendererMixin
 
     @Inject(at = @At("RETURN"), method = "<init>")
     public void onInit(EntityRendererFactory.Context ctx, boolean slim, CallbackInfo ci) {
-        ModelShifterClient.holder = new DynamicAdditionalRendererHolder(ctx, ModelShifterClient.state);
+        ModelShifterClient.holder = new AdditionalRendererManager(ctx, ModelShifterClient.state);
         ModelShifterClient.holder.applyState();
     }
-
-    //? >=1.21.3 {
-    @Inject(at = @At("HEAD"),
-            method = "getPositionOffset(Lnet/minecraft/client/render/entity/state/PlayerEntityRenderState;)Lnet/minecraft/util/math/Vec3d;",
-            cancellable = true)
-    public void injectSetModelPose(PlayerEntityRenderState e, CallbackInfoReturnable<Vec3d> cir) {
-        //?} else {
-    /*@Inject(at = @At("HEAD"),
-            method = "getPositionOffset(Lnet/minecraft/client/network/AbstractClientPlayerEntity;F)Lnet/minecraft/util/math/Vec3d;",
-            cancellable = true)
-    public void injectSetModelPose(AbstractClientPlayerEntity e, float f, CallbackInfoReturnable<Vec3d> cir) {
-    *///?}
-        EntityRenderStateWrapper state = EntityRenderStateWrapper.of(e);
-        if (!state.isPlayer()) return;
-        assert state.getPlayer() != null;
-        if (!ModelShifterClient.state.isRendererEnabled(state.getPlayer())) return;
-
-        //? >=1.21.3 {
-        cir.setReturnValue(super.getPositionOffset(e));
-        //?} else {
-        /*cir.setReturnValue(super.getPositionOffset(e, f));
-         *///?}
-        cir.cancel();
-    }
-
-    //? <1.21.3 {
-    /*@Inject(at = @At("HEAD"),
-            method = "setModelPose",
-            cancellable = true)
-    public void injectSetModelPose(AbstractClientPlayerEntity player, CallbackInfo ci) {
-        MixinUtil.setModelVisibility(player, model);
-        if (ModelShifterClient.state.isRendererEnabled(player))
-            ci.cancel();
-    }
-    *///?}
-
-    //? >=1.21.3 {
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/ModelPart;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;II)V"),
-            method = "renderArm")
-    public void injectRenderArm(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, Identifier skinTexture, ModelPart arm, boolean sleeveVisible, CallbackInfo ci) {
-        //?} else {
-    /*@Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/model/PlayerEntityModel;setAngles(Lnet/minecraft/entity/LivingEntity;FFFFF)V"),
-            method = "renderArm")
-    public void injectRenderArm(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, AbstractClientPlayerEntity player, ModelPart arm, ModelPart sleeve, CallbackInfo ci) {
-    *///?}
-        if (arm == model.leftArm) {
-            model.leftArm.visible = true;
-            model.leftSleeve.visible = true;
-        }
-        if (arm == model.rightArm) {
-            model.rightArm.visible = true;
-            model.rightSleeve.visible = true;
-        }
-    }
-
-    @Inject(at = @At("HEAD"),
-            //? >=1.21.3 {
-            method = "setupTransforms(Lnet/minecraft/client/render/entity/state/PlayerEntityRenderState;Lnet/minecraft/client/util/math/MatrixStack;FF)V",
-            cancellable = true)
-    public void injectSetupTransforms(PlayerEntityRenderState e, MatrixStack matrixStack, float f, float g, CallbackInfo ci) {
-        //?} else if >1.20.4 {
-            /*method = "setupTransforms(Lnet/minecraft/client/network/AbstractClientPlayerEntity;Lnet/minecraft/client/util/math/MatrixStack;FFFF)V",
-            cancellable = true)
-    public void injectSetupTransforms(AbstractClientPlayerEntity e, MatrixStack matrixStack, float f, float g, float h, float i, CallbackInfo ci) {
-            *///?} else {
-            /*method = "setupTransforms(Lnet/minecraft/client/network/AbstractClientPlayerEntity;Lnet/minecraft/client/util/math/MatrixStack;FFF)V",
-            cancellable = true)
-    public void injectSetupTransforms(AbstractClientPlayerEntity e, MatrixStack matrixStack, float f, float g, float h, CallbackInfo ci) {
-            *///?}
-
-        EntityRenderStateWrapper state = EntityRenderStateWrapper.of(e);
-        if (!state.isPlayer()) return;
-        assert state.getPlayer() != null;
-        if (!ModelShifterClient.state.isRendererEnabled(state.getPlayer())) return;
-
-        //? >=1.21.3 {
-        super.setupTransforms(e, matrixStack, f, g);
-        //?} else if >1.20.4 {
-        /*super.setupTransforms(e, matrixStack, f, g, h, i);
-         *///?} else {
-        /*super.setupTransforms(e, matrixStack, f, g, h);
-         *///?}
-        ci.cancel();
-    }
-
-    //? <1.21.3 {
-    /*@Inject(at = @At("HEAD"),
-            method = "render(Lnet/minecraft/client/network/AbstractClientPlayerEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V")
-    public void render(AbstractClientPlayerEntity clientPlayer, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
-        if (!ModelShifterClient.state.isRendererEnabled(clientPlayer)
-                || clientPlayer.isSpectator()) return;
-
-        PlayerModel playerModel = ModelShifterClient.state.getState(clientPlayer.getUuid()).getPlayerModel();
-        ReplacedPlayerEntityRenderer renderer = ModelShifterClient.holder.getRenderer(playerModel);
-        if (renderer != null)
-            renderer.render(clientPlayer, getTexture(clientPlayer), g, matrixStack, vertexConsumerProvider, i);
-    }
-    *///?}
 }
